4676454c7a01b5f1816fa59fad0ed441656b99c2
khushi 06 <khushipandey06844@gmail.com>
2025-06-20 21:44:07 +0530

    view pages added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -76,6 +76,9 @@ export default function Navbar() {
                 More
               </Link>
             </li>
+
+
+            
           </ul>
         </nav>
 
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -11,6 +11,7 @@ export default function ToolsPage() {
   return (
     <>
       <Navbar />
+      
       <AllTools />
     </>
   )
