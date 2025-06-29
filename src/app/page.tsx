c6c048897fb032e193d3a4a00601dbdd84321290
saurabhcodes25 <saurabhjha2001@gmail.com>
2025-06-29 11:54:23 +0530

    editpdf implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
 
 
-
-
-
-
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+/>
 
 export default function Home() {
   return (
